3e17045e6c9680062bb2f25c1b7a73ebdf0809f1
ragul-arvinth-opentext <rarvinth@opentext.com>
2025-06-26 15:59:30 +0530

    added data for demo

--- utilities/data_loader.py
+++ utilities/data_loader.py
@@ -1,6 +1,6 @@
 # utilities/data_loader.py
 import json
 
-def load_test_data(filepath="test_data/test_data.json"):
+def load_test_data(filepath="test_data/test_data_demo.json"):
     with open(filepath) as f:
         return json.load(f)
